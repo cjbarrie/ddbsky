817a4d8604fd5d9c6fc16f71e2f8179192694c7a
cjbarrie <chrisjbarrie@googlemail.com>
2024-11-20 19:53:51 +0200

    Update follows collection script

--- 02_get_uk_sample_follows.R
+++ 02_get_uk_sample_follows.R
@@ -26,21 +26,28 @@ userinfo <- readRDS(userinfo_file)
 
 # Ensure the dataset contains the `actor_handle` column
 if (!"actor_handle" %in% names(userinfo)) {
-  stop("The userinfo dataset must contain an 'author_handle' column.")
+  stop("The userinfo dataset must contain an 'actor_handle' column.")
 }
 
 # Process each user to get and save follows
 for (user in userinfo$actor_handle) {
+  # Check if the user's follows data already exists
+  user_dir <- file.path(base_dir, paste0(user, "_", collection_date))
+  follows_path <- file.path(user_dir, paste0("follows_", collection_date, ".rds"))
+  
+  if (file.exists(follows_path)) {
+    message(sprintf("Follows for user %s already collected. Skipping.", user))
+    next
+  }
+  
   tryCatch({
     # Get follows
     follows <- get_follows(actor = user, limit = 10000L)
     
     # Create a directory for the user with the date suffix
-    user_dir <- file.path(base_dir, paste0(user, "_", collection_date))
     if (!dir.exists(user_dir)) dir.create(user_dir, recursive = TRUE)
     
     # Save the follows data
-    follows_path <- file.path(user_dir, paste0("follows_", collection_date, ".rds"))
     safe_save_rds(data.frame(follows), follows_path)
     
     # Message for successful processing
